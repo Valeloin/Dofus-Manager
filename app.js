b89a045be48af96c932e243865641c01538200d6
Valentin Condamy <v.condamy@gmail.com>
2023-05-23 12:16:07 +0200

    mobile_first_1

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
   var i=1;
  $("#add_row").click(function(){
-  $('#addr'+i).html("<td>"+ (i+1) +"</td><td><input name='ID"+i+"' type='text' placeholder='ID' class='square-card input-md'  /></td><td><input  name='Name"+i+"' type='text' placeholder='Name'  class='square-card input-md'></td><td><input  name='Classe"+i+"' type='text' placeholder='Classe'  class='square-card input-md'></td><td><input  name='LVL"+i+"' type='text' placeholder='LVL'  class='square-card input-md'></td><td><input  name='Details"+i+"' type='text' placeholder='Details'  class='square-card input-md'></td>");
+  $('#addr'+i).html("<td>"+ (i+1) +"</td><td><input name='ID"+i+"' type='text' placeholder='ID' class='form-input-card input-md'  /></td><td><input  name='Name"+i+"' type='text' placeholder='Name'  class='form-input-card input-md'></td><td><input  name='Classe"+i+"' type='text' placeholder='Classe'  class='form-input-card input-md'></td><td><input  name='LVL"+i+"' type='text' placeholder='LVL'  class='form-input-card input-md'></td><td><input  name='Details"+i+"' type='text' placeholder='Details'  class='form-input-card input-md'></td>");
 
-  $('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
+  $('#tab_logic').append('<tr id="addr'+(i+1)+'" ></tr>',);
   i++; 
 });
  $("#delete_row").click(function(){
